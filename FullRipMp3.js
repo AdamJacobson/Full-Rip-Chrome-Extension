bb42a4c5b2b3e1b7c8a185cfe9c06a5bec1151fc
adam <amjacobson0@gmail.com>
2017-09-15 08:45:20 -0700

    Replaced video id extraction with regular expression

--- FullRipMp3.js
+++ FullRipMp3.js
@@ -1,26 +1,18 @@
-var getList = function(url, gkey){
-        var returned = null;
-        if (url.indexOf("?") != -1){
-          var list = url.split("?")[1].split("&"),
-                  gets = [];
-          for (var ind in list){
-            var kv = list[ind].split("=");
-            if (kv.length>0)
-                gets[kv[0]] = kv[1];
-        }
-        returned = gets;
-        if (typeof gkey != "undefined")
-            if (typeof gets[gkey] != "undefined")
-                returned = gets[gkey];
-        }
-        return returned;
+const getVideoId = function() {
+  const idRegex = /\?v=([a-zA-Z]*)&/;
+  const url = document.URL;
+  return idRegex.exec(url)[1];
 };
+
 (function () {
-	var videoid = getList(document.URL, "v");
-	var mp3path ='http://www.fullrip.net/mp3/'+encodeURIComponent(videoid);
-	var videopath = 'http://www.fullrip.net/video/'+encodeURIComponent(videoid);
-	var div_embed = document.getElementById('watch7-subscription-container');
+  var videoId = getVideoId();
+  console.log("Full Rip Ext Loaded with video id: " + videoId);
+
+  var mp3path ='http://www.fullrip.net/mp3/' + encodeURIComponent(videoId);
+  var videopath = 'http://www.fullrip.net/video/' + encodeURIComponent(videoId);
+  var div_embed = document.getElementById('watch7-subscription-container');
+
    if (div_embed) {
-    	div_embed.innerHTML = div_embed.innerHTML + '&nbsp;&nbsp;<input type="button" onClick="javascript:window.open(\''+ mp3path +'\');" value="Get Mp3!"></button>&nbsp;<input type="button" onClick="javascript:(a = (b = document).createElement(\'script\')).src = \'http://fullrip.net/bookmark.js\', b.body.appendChild(a);void(0);" value="Get Video!"></button>';
+    div_embed.innerHTML = div_embed.innerHTML + '&nbsp;&nbsp;<input type="button" onClick="javascript:window.open(\''+ mp3path +'\');" value="Get Mp3!"></button>&nbsp;<input type="button" onClick="javascript:(a = (b = document).createElement(\'script\')).src = \'http://fullrip.net/bookmark.js\', b.body.appendChild(a);void(0);" value="Get Video!"></button>';
   }
 })();
